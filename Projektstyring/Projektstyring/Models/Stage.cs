3cecab40ec8c5b7b86b96eb1b1f625a5e700c083
Dina Sakæussen <disa5@student.eadania.dk>
2025-10-28 13:53:43 +0100

    lavet stage

---
+++ Projektstyring/Projektstyring/Models/Stage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektstyring.Models
+{
+    class Stage
+    {
+        string title;
+        List<Task> tasks = new List<Task>();
+        Color backgroundColor;
+
+        public Stage(string titleInput, Color colorInput)
+        {
+            title = titleInput;
+            backgroundColor = colorInput;
+        }
+    }
+}
